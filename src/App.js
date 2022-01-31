473a86b3bd14c76d6eaa6ee3ac22b75841bca1b5
Kritsada Tariya <mr.kritsada.t@gmail.com>
2022-01-31 19:19:33 +0700

    :pencil2: implement form crud done !

--- src/App.js
+++ src/App.js
@@ -3,11 +3,66 @@ import axios from "axios";
 import React, { useState } from "react";
 
 function App() {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [country, setCountry] = useState("");
+    const [position, setPosition] = useState("");
+    const [wage, setWage] = useState(0);
+    const [newWage, setNewWage] = useState(0);
+
     const [employeeList, setEmployeeList] = useState([]);
 
     const getEmployees = () => {
-        axios.get("http://localhost:3001/employees").then((resp) => {
+        axios.get("http://localhost:3001/").then((resp) => {
             setEmployeeList(resp.data);
+        });
+    };
+
+    const addEmployee = () => {
+        axios
+            .post("http://localhost:3001/create", {
+                name,
+                age,
+                country,
+                position,
+                wage,
+            })
+            .then(() => {
+                setEmployeeList([
+                    ...employeeList,
+                    { name, age, country, position, wage },
+                ]);
+            });
+    };
+
+    const updateEmployeeWage = (id) => {
+        axios
+            .put("http://localhost:3001/update", { wage: newWage, id: id })
+            .then((resp) => {
+                setEmployeeList(
+                    employeeList.map((val) => {
+                        return val.id === id
+                            ? {
+                                  id: val.id,
+                                  name: val.name,
+                                  age: val.age,
+                                  country: val.country,
+                                  position: val.position,
+                                  wage: newWage,
+                              }
+                            : val;
+                    })
+                );
+            });
+    };
+
+    const deleteEmployeeWage = (id) => {
+        axios.delete(`http://localhost:3001/delete/ ${id}`).then((resp) => {
+            setEmployeeList(
+                employeeList.filter((val) => {
+                    return val.id !== id;
+                })
+            );
         });
     };
 
@@ -24,6 +79,9 @@ function App() {
                             type="text"
                             className="form-control"
                             placeholder="Enter name"
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
                         />
                     </div>
                     <div className="mb-3">
@@ -34,6 +92,9 @@ function App() {
                             type="number"
                             className="form-control"
                             placeholder="Enter age"
+                            onChange={(e) => {
+                                setAge(e.target.value);
+                            }}
                         />
                     </div>
                     <div className="mb-3">
@@ -44,6 +105,9 @@ function App() {
                             type="text"
                             className="form-control"
                             placeholder="Enter country"
+                            onChange={(e) => {
+                                setCountry(e.target.value);
+                            }}
                         />
                     </div>
                     <div className="mb-3">
@@ -54,6 +118,9 @@ function App() {
                             type="text"
                             className="form-control"
                             placeholder="Enter position"
+                            onChange={(e) => {
+                                setPosition(e.target.value);
+                            }}
                         />
                     </div>
                     <div className="mb-3">
@@ -64,9 +131,14 @@ function App() {
                             type="number"
                             className="form-control"
                             placeholder="Enter wage"
+                            onChange={(e) => {
+                                setWage(e.target.value);
+                            }}
                         />
                     </div>
-                    <button className="btn btn-success">Add Employee</button>
+                    <button className="btn btn-success" onClick={addEmployee}>
+                        Add Employee
+                    </button>
                 </form>
             </div>
             <hr />
@@ -78,7 +150,7 @@ function App() {
                 <br />
                 {employeeList.map((val, key) => {
                     return (
-                        <div className="employee card">
+                        <div className="employee card mb-5" key={key}>
                             <div className="card-body text-start">
                                 <p className="card-texr">Name: {val.name}</p>
                                 <p className="card-texr">Age: {val.age}</p>
@@ -89,6 +161,33 @@ function App() {
                                     Position: {val.position}
                                 </p>
                                 <p className="card-texr">Wage: {val.wage}</p>
+                                <div className="d-flex">
+                                    <input
+                                        type="number"
+                                        placeholder="15000..."
+                                        className="form-control"
+                                        style={{ width: "300px" }}
+                                        onChange={(e) => {
+                                            setNewWage(e.target.value);
+                                        }}
+                                    />
+                                    &nbsp;
+                                    <button
+                                        className="btn btn-warning"
+                                        onClick={() => {
+                                            updateEmployeeWage(val.id);
+                                        }}>
+                                        Update
+                                    </button>
+                                    &nbsp;
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={() => {
+                                            deleteEmployeeWage(val.id);
+                                        }}>
+                                        Delete
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     );
